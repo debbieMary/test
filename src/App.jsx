e71b2084ceb007e10865cb99aaadbbfe139a8b9a
Debbie Zuleta <debbie.zuletam@gmail.com>
2025-09-22 19:43:47 -0400

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Component from './Component'
+import OtherComponent from './OtherComponent'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+   <Component/>
+   <OtherComponent/>
+    </>
+  )
+}
+
+export default App
